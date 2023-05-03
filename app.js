2dc0b974b9ade41195f2b1a10181760f1a3fd91b
pulkit-30 <pulkit30.bsr@gmail.com>
2023-05-03 18:24:06 +0530

    temp commit

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const path = require("path");
-const bodyparser=require('body-parser');
+const bodyparser = require("body-parser");
 
 const app = express();
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "/public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.get("/", (req, res) => {
-  res.render('home');
+  res.render("home");
 });
 app.get("/contact", (req, res) => {
-  res.render('contact');
+  res.render("contact");
 });
-app.post("/constact",(req,res)=>{
-  res.redirect('/');
-})
+app.post("/constact", (req, res) => {
+  res.redirect("/");
+});
 app.listen(80, () => {
+  console.log("temp");
   console.log("running");
 });
